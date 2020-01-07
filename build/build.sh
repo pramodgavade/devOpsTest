f45eb62e553e3f7234f3ef801f1dfe7400729147
pramod_gavade <pramod_gavade@pramod-gavades-MacBook-Pro.local>
2020-01-07 13:30:21 -0800

    build

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,13 @@
 # git commits
+currGitCommit = ${GIT_COMMIT}
+prevGitCommit = ${GIT_PREVIOUS_SUCCESSFUL_COMMIT}
+if [ -z $2] then
+    echo "empty commit id"
+else 
+    echo "commit id: $2"
+fi    
+
+
 echo "GIT_COMMIT = ${GIT_COMMIT}"
 echo "GIT_PREVIOUS_SUCCESSFUL_COMMIT = ${GIT_PREVIOUS_SUCCESSFUL_COMMIT}"
 
@@ -6,36 +15,36 @@ echo "GIT_PREVIOUS_SUCCESSFUL_COMMIT = ${GIT_PREVIOUS_SUCCESSFUL_COMMIT}"
 echo "***************list of modified files***************"
 git diff --name-only ${GIT_COMMIT} ${GIT_PREVIOUS_SUCCESSFUL_COMMIT}
 
-tempDirectory="temp-dir"
+tempDirectory = "temp-dir"
 
 echo "***************building force-app folder***************"
 
 # loop through list of modified files
 for fileName in $(git diff --name-only ${GIT_COMMIT} ${GIT_PREVIOUS_SUCCESSFUL_COMMIT})
-	do
-    	echo "current file : $fileName"
+    do
+        echo "current file : $fileName"
         
         # if file modified file is from force-app/main/default
         if [[ $fileName == *"force-app"* ]]; then
-  			echo "including $fileName"
+            echo "including $fileName"
             
             # First create the target directory, if it doesn't exist
-			mkdir -p "$tempDirectory/$(dirname $fileName)"
+            mkdir -p "$tempDirectory/$(dirname $fileName)"
             
-			# Then copy over the file
-			cp -rf "$fileName" "$tempDirectory/$fileName"
+            # Then copy over the file
+            cp -rf "$fileName" "$tempDirectory/$fileName"
             
             # Then copy over the meta data file if it exists
-            metaFileName="$fileName-meta.xml"
+            metaFileName = "$fileName-meta.xml"
             if [ -f "$metaFileName" ]; then
-    			echo "including $metaFileName"
+                echo "including $metaFileName"
                 cp -rf "$metaFileName" "$tempDirectory/$metaFileName"
             fi    
         else 
-        	echo "skipped $fileName"
-		fi
+            echo "skipped $fileName"
+        fi
         
-	done
+    done
     
 # navigate to workspace
 cd $WORKSPACE
